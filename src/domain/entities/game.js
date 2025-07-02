7ba9fe59ed5d4d2be866675b80d6489f8175e747
Bruno Ortega <brunoxavierortega@gmail.com>
2025-07-01 22:53:02 -0500

    tercero

--- src/domain/entities/game.js
+++
@@ -1,8 +0,0 @@
-export class Game {
-  constructor({ teamA, teamB, scoreA = 0, scoreB = 0 }) {
-    this.teamA = teamA;
-    this.teamB = teamB;
-    this.scoreA = scoreA;
-    this.scoreB = scoreB;
-  }
-}
--- src/domain/usecases/updateScore.js
+++
@@ -1,8 +0,0 @@
-export function updateScore(game, team, points) {
-  if (team === 'A') {
-    game.scoreA += points;
-  } else if (team === 'B') {
-    game.scoreB += points;
-  }
-  return { ...game };
-}
